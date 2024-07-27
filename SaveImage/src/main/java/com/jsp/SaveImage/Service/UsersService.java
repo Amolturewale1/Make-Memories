1f336e953370b042e4d531e4b0a34b7d129cb9f7
Amolturewale1 <turewaleamol@gmail.com>
2024-07-27 21:05:26 +0530

    initial commit.

---
+++ SaveImage/src/main/java/com/jsp/SaveImage/Service/UsersService.java
@@ -0,0 +1,15 @@
+package com.jsp.SaveImage.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.jsp.SaveImage.entity.Users;
+
+@Service
+public interface UsersService {
+	public Users register(Users user);
+	
+	public boolean login(Users user);
+	
+	public Users getUserByEmailAndPassword(Users user);
+
+}
